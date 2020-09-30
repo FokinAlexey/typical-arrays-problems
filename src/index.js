65a051884feccdb1851fb015c656deb7b7acace8
FokinAlexey <a.fokin1979@gmail.com>
2020-09-30 14:59:42 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  let array = [];
+  if array.length === 0 {
+      return 0;
+  } else if {
+      for (i = 0, i < array.length, i++) {
+          typeof array[i] !== 'number';
+          return 0;
+      };
+    };
+    return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    let array = [];
+    if array.length === 0 {
+        return 0;
+    } else if {
+        for (i = 0, i < array.length, i++) {
+            typeof array[i] !== 'number';
+            return 0;
+        };
+      };
+      return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let array = [];
+    if array.length === 0 {
+        return 0;
+    } else if {
+        for (i = 0, i < array.length, i++) {
+            typeof array[i] !== 'number';
+            return 0;
+        };
+      };
+    let result = 0;  
+    for (i = 0, i < array.length, i++) {
+        result += array[i];
+    };
+      return (result / array.length);
 }
