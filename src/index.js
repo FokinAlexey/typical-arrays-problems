a5eed7a489a8bc5f343ad3da6aaff40d97315cd7
FokinAlexey <70858193+FokinAlexey@users.noreply.github.com>
2020-10-01 15:58:10 +0300

    fix: add working solution

--- src/index.js
+++ src/index.js
@@ -1,43 +1,30 @@
-
 exports.min = function min (array) {
-  let array = [];
-  if array.length === 0 {
-      return 0;
-  } else if {
-      for (i = 0, i < array.length, i++) {
-          typeof array[i] !== 'number';
-          return 0;
-      };
+    if (array === undefined || array[0]=== undefined) {
+         return 0;
     };
-    return Math.min.apply(null, array);
+    return Math.min.apply(null, array); 
 }
 
+    
+  
 exports.max = function max (array) {
-    let array = [];
-    if array.length === 0 {
+    if (array === undefined || array[0]=== undefined) {
         return 0;
-    } else if {
-        for (i = 0, i < array.length, i++) {
-            typeof array[i] !== 'number';
-            return 0;
-        };
-      };
-      return Math.max.apply(null, array);
+    };
+    return Math.max.apply(null, array); 
 }
+ 
 
 exports.avg = function avg (array) {
-    let array = [];
-    if array.length === 0 {
+    if (array === undefined || array[0]=== undefined) {
         return 0;
-    } else if {
-        for (i = 0, i < array.length, i++) {
-            typeof array[i] !== 'number';
-            return 0;
-        };
-      };
-    let result = 0;  
-    for (i = 0, i < array.length, i++) {
-        result += array[i];
+    };   
+    
+     let avg = 0;
+        for(var i=0; i<array.length; i++) {
+        avg += array[i];
     };
-      return (result / array.length);
+        
+    avg = avg/array.length;
+        return avg; 
 }
